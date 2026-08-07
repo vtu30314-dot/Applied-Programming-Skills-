886230732ef5d65438a845f08e754f0a84d10dc2
vtu30314-dot <vtu30314@veltech.edu.in>
2026-08-07 10:29:00 +0530

    Time: 30 ms (19.2%), Space: 92.1 MB (33.45%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,19 +1,20 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
+         HashSet<Integer> set = new HashSet<>();
+
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                if (i - map.get(nums[i]) <= k) {
-                    return true;
-                }
+
+            if (set.contains(nums[i])) {
+                return true;
             }
-            map.put(nums[i], i);
+
+            set.add(nums[i]);
+
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
         }
-        
+
         return false;
     }
 }
